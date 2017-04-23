8ee1031b8d9c4e491aec4cc97a5ced069b390475
Dan Bravender <dan.bravender@gmail.com>
2017-04-23 08:22:38 -1000

    Bump to 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='mittmcts',
-    version='0.2',
+    version='0.3',
     description='Man in the Table Information Set Monte Carlo Tree Search '
                 'library',
     author='Dan Bravender',
